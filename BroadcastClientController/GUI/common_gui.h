153ff53d377d873a6d6d83a3374ae572b06e507e
Valik <valikl_75@yahoo.com>
2013-05-03 05:19:35 +0300

    common GUI settings

---
+++ BroadcastClientController/GUI/common_gui.h
@@ -0,0 +1,10 @@
+#ifndef COMMON_GUI_H
+#define COMMON_GUI_H
+
+#include <QtGui>
+#include "BBTT/BB_ConfigMgr.h"
+
+#define GRID(layout) ((QGridLayout*)(layout))
+#define ConfigMgr (BB_ConfigMgr::Instance())
+
+#endif
